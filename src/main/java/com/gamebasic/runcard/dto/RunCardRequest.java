910b8d98a51e0460878be80f329966b6496eb5d7
dingokim2 <alvin0406@naver.com>
2026-09-08 17:35:31 +0900

    Lv 5. Bean Validation: 게임 생성

--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,15 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
     // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    @NotEmpty
     private String cardType;
+    @Min(0) @Max(10)
     private Integer acquiredFloor;
 }
