a853f0483d92e9a4e3da694fde6ed39f44d33a6d
dingokim2 <alvin0406@naver.com>
2026-09-09 11:56:48 +0900

    Lv 7. 보상 카드 선택과 진행 저장
    GameService.getGames 에서 반환하는 리스트의 각 GameSummaryResponse 원소는 deckSize = -1, createAt = "", updateAt = "" 상태이다.

--- src/main/java/com/gamebasic/runcard/repository/RunCardRepository.java
+++ src/main/java/com/gamebasic/runcard/repository/RunCardRepository.java
@@ -1,6 +1,7 @@
 package com.gamebasic.runcard.repository;
 
 import com.gamebasic.game.entity.Game;
+import com.gamebasic.runcard.dto.CardResponse;
 import com.gamebasic.runcard.entity.RunCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,6 +11,8 @@ public interface RunCardRepository extends JpaRepository<RunCard, Long> {
     List<RunCard> findAllByGameOrderByIdAsc(Game game);
 
     void deleteAllByGame(Game game);
+
+    List<CardResponse> findAllByGameIdOrderByIdAsc(Long gameId);
 
     // TODO (Lv 11): @Query 작성
     // List<DeckCount> countByGames(List<Game> games);
